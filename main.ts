233e96c8a153494ae829cd13b7fb7a1ef308dec2
yessheo <94099635+yessheo@users.noreply.github.com>
2021-11-11 16:11:14 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.giggle.playUntilDone()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.twinkle.playUntilDone()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.soaring.playUntilDone()
+})
 basic.forever(function () {
 	
 })
